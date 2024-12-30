9ede8c9b4772430e4cea8655b290a66b133eaff5
Surendar Yama <surendaryama@outlook.com>
2024-12-30 15:26:41 +0530

    Components  ->  inputs  ->  input aliases done

--- src/app/shared/components/components-tuts/components-tuts.component.ts
+++ src/app/shared/components/components-tuts/components-tuts.component.ts
@@ -1,7 +1,24 @@
-import { Component } from "@angular/core";
+import {
+  booleanAttribute,
+  Component,
+  input,
+  numberAttribute,
+} from "@angular/core";
 
 @Component({
   selector: "app-components-tuts",
   templateUrl: "./components-tuts.component.html",
 })
-export class ComponentsTutsComponent {}
+export class ComponentsTutsComponent {
+  value = input("", {
+    alias: "name",
+    transform: (value: string | undefined): string =>
+      value?.toUpperCase() ?? "",
+  });
+
+  width = input(0, {
+    transform: numberAttribute,
+  });
+
+  isAuth = input(false, { transform: booleanAttribute });
+}
